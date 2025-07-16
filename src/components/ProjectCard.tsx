17ca1d369b899b5269099cf28846b7edd3ce30d9
Jacopo Sassi <sassijacopp@gmail.com>
2025-07-16 17:51:52 +0200

    feat: restructure app layout and styles

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,16 @@
+import classes from "./ProjectCard.module.css";
+
+export function ProjectCard() {
+  return (
+    <section className={classes.project}>
+      <h2>Project Name</h2>
+      <div className={classes.tasks}>
+        <ul>
+          <li>Task 1</li>
+          <li>Task 2</li>
+          <li>Task 3</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
