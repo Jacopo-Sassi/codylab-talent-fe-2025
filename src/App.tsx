b9db91c85e2bf26550c912550bcade29750c91db
Jacopo Sassi <sassijacopp@gmail.com>
2025-07-21 12:54:29 +0200

    feat: add routing and new pages for Projects and Workload
    - Integrated react-router-dom for navigation
    - Created Projects and Workload pages with respective components
    - Updated Navigation component to use Link for routing
    - Added Projects and Workload CSS modules
    - Updated App component to include routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import classes from "./App.module.css";
 import { Header } from "./components/Header";
-import { ProjectList } from "./components/ProjectList";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Workload } from './pages/Workload';
+import { Projects } from './pages/Projects';
 
 function App() {
   return (
-     <div className={classes.app}>
-      <Header />
-      <main className={classes.main}>
-       <ProjectList />
-      </main>
+    <div className={classes.app}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/workload" element={<Workload />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Workload.tsx
@@ -0,0 +1,10 @@
+
+import classes from "./Workload.module.css";
+export function Workload() {
+  return (
+    <div>
+      <h1>Workload Page</h1>
+      <p>This is the Workload page content</p>
+    </div>
+  );
+}
