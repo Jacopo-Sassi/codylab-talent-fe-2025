f10d06d7e46c5d6321f31eefd903a3a88ef9ab69
Pietro Saccomando <pietro.saccomando@intesys.it>
2025-07-22 09:49:03 +0200

    fix(Actions): replace anchor with Link component for routing

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -1,11 +1,12 @@
 import classes from './Actions.module.css';
+import { Link } from "react-router-dom";
 
 export function Actions() {
   return (
     <div className={classes.actions}>
       <ul>
         <li>
-          <a href="/add-project">Nuovo progetto</a>
+          <Link to="/add-project" className={classes.link}>Nuovo progetto</Link>
         </li>
       </ul>
     </div>
